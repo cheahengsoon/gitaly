f22a276f60969681f73969075a5b6987ba4c9a60
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-01-11 14:53:43 +0100

    cmd/gitaly-wrapper: Add tests verifying the executable works as expected
    While we now have a bunch of tests which verify that low-level
    components of gitaly-wrapper work as expected, we have no high-level
    tests which verify that it comes together nicely.
    
    Add a bunch of tests which verify that running the gitaly-wrapper
    produces desired results.

--- cmd/gitaly-wrapper/main_test.go
+++ cmd/gitaly-wrapper/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -9,7 +11,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/bootstrap"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper/testcfg"
 )
 
 // TestStolenPid tests for regressions in https://gitlab.com/gitlab-org/gitaly/issues/1661
@@ -218,4 +222,103 @@ func TestIsProcessAlive(t *testing.T) {
 	process, err := os.FindProcess(77777777)
 	require.NoError(t, err)
 	require.False(t, isProcessAlive(process))
+}
+
+func TestRun(t *testing.T) {
+	ctx, cancel := testhelper.Context()
+	defer cancel()
+
+	binary := testcfg.BuildGitalyWrapper(t, testcfg.Build(t))
+
+	t.Run("missing arguments", func(t *testing.T) {
+		output, err := exec.CommandContext(ctx, binary).CombinedOutput()
+		require.Error(t, err)
+		require.Contains(t, string(output), "usage: ")
+	})
+
+	t.Run("missing PID file envvar", func(t *testing.T) {
+		output, err := exec.CommandContext(ctx, binary, "binary").CombinedOutput()
+		require.Error(t, err)
+		require.Contains(t, string(output), "missing pid file ENV variable")
+	})
+
+	t.Run("invalid executable", func(t *testing.T) {
+		pidPath := filepath.Join(testhelper.TempDir(t), "pid")
+
+		cmd := exec.CommandContext(ctx, binary, "does-not-exist")
+		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", bootstrap.EnvPidFile, pidPath))
+
+		output, err := cmd.CombinedOutput()
+		require.Error(t, err)
+		require.Contains(t, string(output), "executable file not found in $PATH")
+	})
+
+	t.Run("adopting executable", func(t *testing.T) {
+		script := testhelper.WriteExecutable(t, filepath.Join(testhelper.TempDir(t), "script"), []byte(
+			`#!/usr/bin/env bash
+			sleep 1
+		`))
+
+		scriptCmd := exec.CommandContext(ctx, script)
+		require.NoError(t, scriptCmd.Start())
+		go func() {
+			require.NoError(t, scriptCmd.Wait())
+		}()
+
+		// Write the PID of the running process into the PID file. As a result, it should
+		// get adopted by gitaly-wrapper, which means it wouldn't try to execute it anew.
+		pidPath := filepath.Join(testhelper.TempDir(t), "pid")
+		require.NoError(t, os.WriteFile(pidPath, []byte(strconv.FormatInt(int64(scriptCmd.Process.Pid), 10)), 0o644))
+
+		var output bytes.Buffer
+		// Run gitaly-script with a binary path whose basename matches, but which ultimately
+		// doesn't exist. This proves that it doesn't try to execute the script again.
+		cmd := exec.CommandContext(ctx, binary, "/does/not/exist/bash")
+		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", bootstrap.EnvPidFile, pidPath))
+		cmd.Stdout = &output
+		cmd.Stderr = &output
+		require.NoError(t, cmd.Start())
+
+		// The wrapper shall exit when the wrapped script does.
+		require.NoError(t, cmd.Wait(), output.String())
+		require.Contains(t, output.String(), "adopting a process")
+		require.Contains(t, output.String(), "wrapper for process shutting down")
+	})
+
+	t.Run("spawning executable", func(t *testing.T) {
+		script := testhelper.WriteExecutable(t, filepath.Join(testhelper.TempDir(t), "script"), []byte(
+			`#!/usr/bin/env bash
+			echo "I have been executed"
+		`))
+
+		pidPath := filepath.Join(testhelper.TempDir(t), "pid")
+
+		cmd := exec.CommandContext(ctx, binary, script)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", bootstrap.EnvPidFile, pidPath))
+		output, err := cmd.CombinedOutput()
+		require.NoError(t, err)
+
+		require.Contains(t, string(output), "spawning a process")
+		require.Contains(t, string(output), "I have been executed")
+
+		require.NoFileExists(t, pidPath)
+	})
+
+	t.Run("spawning executable with dead process", func(t *testing.T) {
+		script := testhelper.WriteExecutable(t, filepath.Join(testhelper.TempDir(t), "script"), []byte(
+			`#!/usr/bin/env bash
+			echo "I have been executed"
+		`))
+
+		pidPath := filepath.Join(testhelper.TempDir(t), "pid")
+		require.NoError(t, os.WriteFile(pidPath, []byte("12345"), 0o644))
+
+		cmd := exec.CommandContext(ctx, binary, script)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", bootstrap.EnvPidFile, pidPath))
+		output, err := cmd.CombinedOutput()
+		require.NoError(t, err)
+
+		require.Contains(t, string(output), "spawning a process")
+		require.Contains(t, string(output), "I have been executed")
+	})
 }
