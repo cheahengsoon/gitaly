9666bf5e57f57f14d9ffd24f90cfcb989f78090d
Jacob Vosmaer <jacob@gitlab.com>
2022-01-12 13:13:40 +0000

    client: add UploadPackWithSidechannel
    This adds UploadPackWithSidechannel which will be used internally by
    gitaly-ssh, and externally by gitlab-shell.

--- client/upload_pack.go
+++ client/upload_pack.go
@@ -2,8 +2,10 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"io"
 
+	"gitlab.com/gitlab-org/gitaly/v14/internal/git/pktline"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/stream"
 	"gitlab.com/gitlab-org/gitaly/v14/proto/go/gitalypb"
 	"gitlab.com/gitlab-org/gitaly/v14/streamio"
@@ -39,4 +41,72 @@ func UploadPack(ctx context.Context, conn *grpc.ClientConn, stdin io.Reader, std
 			errC <- errRecv
 		}
 	}, stdout, stderr)
+}
+
+// UploadPackWithSidechannel proxies an SSH git-upload-pack (git fetch)
+// session to Gitaly using a sidechannel for the raw data transfer.
+func UploadPackWithSidechannel(ctx context.Context, conn *grpc.ClientConn, reg *SidechannelRegistry, stdin io.Reader, stdout, stderr io.Writer, req *gitalypb.SSHUploadPackWithSidechannelRequest) (int32, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	copyRequest := func(c SidechannelConn) error {
+		if _, err := io.Copy(c, stdin); err != nil {
+			return fmt.Errorf("copy request: %w", err)
+		}
+		if err := c.CloseWrite(); err != nil {
+			return fmt.Errorf("close request: %w", err)
+		}
+		return nil
+	}
+
+	copyResponse := func(c SidechannelConn) error {
+		if err := pktline.EachSidebandPacket(c, func(band byte, data []byte) (err error) {
+			switch band {
+			case 1:
+				_, err = stdout.Write(data)
+			case 2:
+				_, err = stderr.Write(data)
+			default:
+				err = fmt.Errorf("unexpected band: %d", band)
+			}
+			return err
+		}); err != nil {
+			return fmt.Errorf("copy response: %w", err)
+		}
+
+		return nil
+	}
+
+	ctx, wt := reg.Register(ctx, func(c SidechannelConn) error {
+		request := make(chan error, 1)
+		go func() { request <- copyRequest(c) }()
+
+		response := make(chan error, 1)
+		go func() { response <- copyResponse(c) }()
+
+		for {
+			select {
+			case err := <-response:
+				// Server is done. No point in waiting for client.
+				return err
+			case err := <-request:
+				// Client is done; wait for server to finish too.
+				if err != nil {
+					return err
+				}
+			}
+		}
+	})
+	defer wt.Close()
+
+	sshClient := gitalypb.NewSSHServiceClient(conn)
+	if _, err := sshClient.SSHUploadPackWithSidechannel(ctx, req); err != nil {
+		return 0, err
+	}
+
+	if err := wt.Close(); err != nil {
+		return 0, err
+	}
+
+	return 0, nil
 }
